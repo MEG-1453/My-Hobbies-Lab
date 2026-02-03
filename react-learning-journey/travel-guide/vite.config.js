3a1ebee94fe1eaac974f836d3c1e7eb435a3d031
Muhammet Emin Genç <muhammetemingenc679@gmail.com>
2026-02-03 05:18:56 +0300

    Gerekli Düzenlemeler Yapılıyor

--- react-learning-journey/travel-guide/vite.config.js
+++ react-learning-journey/travel-guide/vite.config.js
@@ -4,4 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/My-Hobbies-Lab/react-learning-journey/travel-guide/', // Bu satırı ekledim
 })
